c77faab295ab8ddd3caebafaf218d8815c57a319
Ivan Shumilov <ivanshum@mustbefamily.com>
2024-04-10 18:25:10 +0200

    set new files

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import { RGBELoader } from 'three-stdlib'
 
 function Ring(props) {
   const texture2 = useLoader(RGBELoader, '/1.hdr')
-  const texture = useLoader(RGBELoader, '/2.hdr')
   const ref = useRef()
-  const { nodes, materials } = useGLTF('/ringwse.glb')
+  const { nodes } = useGLTF('/camni.glb')
   const dia = useControls('dia', {
     bounces: { value: 2, min: 0, max: 8, step: 1 },
     aberrationStrength: { value: 0.01, min: 0, max: 0.1, step: 0.01 },
@@ -25,38 +24,46 @@ function Ring(props) {
     color: 'blue',
     fastChroma: true
   })
-  const metal = useControls('metal', {
-    metalness: 1,
-    roughness: 0.1,
-    wireframe: true,
-    color: 'white'
-  })
   return (
-    <group ref={ref} rotation={[-Math.PI / 2, 0, 0]} {...props} scale={0.1}>
-      <mesh receiveShadow castShadow geometry={nodes['Layer_01(1B21DE05-E3AC-4C62-9547-4FFBA3C8A566)_1'].geometry}>
+    <group ref={ref} rotation={[-Math.PI / 2, 0, 0]} {...props}>
+    <mesh
+      castShadow
+      receiveShadow
+      geometry={nodes['Layer_01(1B21DE05-E3AC-4C62-9547-4FFBA3C8A566)'].geometry}
+      >
         <MeshRefractionMaterial envMap={texture2} {...dia} />
       </mesh>
-      <mesh receiveShadow castShadow geometry={nodes['Layer_01(822F5737-2B27-440C-B7D3-13B5C206F91D)_1'].geometry}>
+    <mesh
+      castShadow
+      receiveShadow
+      geometry={nodes['Layer_01(822F5737-2B27-440C-B7D3-13B5C206F91D)'].geometry}
+      >
         <MeshRefractionMaterial envMap={texture2} {...cnt} />
       </mesh>
-    </group>
+  </group>
   )
 }
-function Suzi(props) {
-  const { scene, materials } = useGLTF('/rbk.glb')
+
+function Metal(props) {
+  const { scene } = useGLTF('/metal.glb')
   useLayoutEffect(() => {
     scene.traverse((obj) => obj.isMesh && (obj.receiveShadow = obj.castShadow = true))
   })
   return <primitive object={scene} {...props} />
 }
+
 export default function App() {
-  const texture = useLoader(RGBELoader, '/1.hdr')
+  const envMap = useControls('Environment', {
+    background: false,
+    files: "/1.hdr",
+    ground: { scale: 50 }
+  })  
   return (
     <Canvas camera={{ position: [-5, 0.5, 5], fov: 45 }}>
-      <Environment files="/night.hdr" ground={{ scale: 10 }} />
+      <Metal  scale={100}/>
+      <Ring   scale={0.1} />
       <ambientLight color={'white'} intensity={4} />
-      <Suzi scale={100} />
-      <Ring rotation={[0, 0, 0.715]} position={[0, -0.175 + 0.5, 0]} />
+      <Environment {...envMap}/>
       <OrbitControls makeDefault autoRotate autoRotateSpeed={0.1} minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
     </Canvas>
   )
